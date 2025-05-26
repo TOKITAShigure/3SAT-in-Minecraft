439f99792bfc12170566d12ce1a23a41c557ea5e
TOKITAShigure <s1300226@u-aizu.ac.jp>
2025-05-26 16:09:58 +0900

    add application source code

---
+++ ThreeSat2MineApp.py
@@ -0,0 +1,36 @@
+from src.ThreeSat2Mine import ThreeSat2Mine
+
+
+# 論理式の詳細の入力
+var_num=input("Input the number of variables : ") # 変数の数(種類)を入力
+cla_num=input("Input the number of clauses : ") # 節の数を入力
+
+T2M=ThreeSat2Mine(var_num,cla_num);
+T2M.setVariables()
+T2M.setClauses()
+
+for i in range(int(cla_num)):
+    
+    print('please input clause ',i+1,': ')
+    var_list=[] 
+    v1,v2,v3=(int(x) for x in input().split())
+    
+    if(v1>=0):
+        var_list.append([v1,True])
+    else:
+        var_list.append([v1,False])
+        
+    if(v2>=0):
+        var_list.append([v2,True])
+    else:
+        var_list.append([v2,False])
+        
+    if(v3>=0):
+        var_list.append([v3,True])
+    else:
+        var_list.append([v3,False])            
+    
+    T2M.setLogic(i,var_list[0],var_list[1],var_list[2])  
+
+T2M.setRedStone()
+del T2M
